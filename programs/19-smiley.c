86da16d451d169abd2693399becb364f79224589
beingprogrammed <tghanshyam512@gmail.com>
2025-11-19 11:51:52 +0530

    Hehehe, another one

--- programs/19-smiley.c
+++ programs/19-smiley.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 int main() {
 
-    //this is a modern gcc compiler so printing ASCII value 1 may not show a smiling face at it uses utf-8 encoding, in which ASCII value 1 is a control character, which is not printable, and that character is used for "Start of Heading" in text communication.
-    //but in older compilers like turbo C it shows a smiling face.
+    //i am on arch, this doesnt use cp437 encoding, so smiling face may not appear correctly.
 
+    //we need a terminal that uses CP437 encoding (like old DOS terminals) to see the smiling face character.
+
+    //maybe will work on windows cmd or powershell
     int i, j;
     for (i = 0; i < 25; i++) { // Assuming 25 rows
         for (j = 0; j < 80; j++) { // Assuming 80 columns
