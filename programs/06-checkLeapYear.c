bc3b4703b13836fa6fa2315d420370e3ba99d7cc
beingprogrammed <tghanshyam512@gmail.com>
2025-11-19 09:47:10 +0530

    Changed question no 6. Leap check with conditional operators

--- programs/06-checkLeapYear.c
+++ programs/06-checkLeapYear.c
@@ -6,11 +6,10 @@ int main() {
     printf("Enter a year: ");
     scanf("%d", &year);
 
-    // Leap year logic:
-    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
-        printf("%d is a leap year.\n", year);
-    else
-        printf("%d is not a leap year.\n", year);
+    // Using conditional (ternary) operator:
+    ( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )
+        ? printf("%d is a leap year.\n", year)
+        : printf("%d is not a leap year.\n", year);
 
     return 0;
 }
