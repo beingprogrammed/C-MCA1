31c082c5f70b047c34bb672244f5edf85435d17f
beingprogrammed <tghanshyam512@gmail.com>
2025-11-20 10:17:18 +0530

    forgot to add zeroCount in 27th program

--- programs/27-oddevenposneg.c
+++ programs/27-oddevenposneg.c
@@ -1,26 +1,26 @@
 //WAP to find out in a[25] how many are positive, how many are negative, how many are even and how many odd.
 #include <stdio.h>
 int main() {
-    int arr[25];
+    int a[25];
     int i;
     int positiveCount = 0, negativeCount = 0, evenCount = 0, oddCount = 0, zeroCount = 0;  
     printf("Enter 25 integers (positive and negative):\n");
     for (i = 0; i < 25; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &a[i]);
     }
     for (i = 0; i < 25; i++) {
 
         //pos neg check
-        if (arr[i]==0)
+        if (a[i]==0)
             zeroCount++;
-        else if (arr[i] > 0) {
+        else if (a[i] > 0) {
             positiveCount++;
-        } else if (arr[i] < 0) {
+        } else if (a[i] < 0) {
             negativeCount++;
         }
 
         //odd even check
-        if (arr[i] % 2 == 0) {
+        if (a[i] % 2 == 0) {
             evenCount++;
         } else {
             oddCount++;
